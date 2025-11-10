7de0cf724e78a8152daa883ef2a3f28dce6a3455
Janne Aleksander Grimm <jannealeksandergrimm@mac.speedport.ip>
2025-11-10 23:28:07 +0100

    build module for clean code, build functions for loading different images, moving these images, randomly choose which images are displayed

--- Choose_Fruit.py
+++ Choose_Fruit.py
@@ -1,24 +1,39 @@
 import pygame
 import sys
 
+from functions import WINDOW_WIDTH, WINDOW_HEIGHT
+from functions import move_item, item_switcher, load_fruit_images
+
+
 
 
 pygame.init()
-screen = pygame.display.set_mode((1400, 900))
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
-bg = pygame.image.load("images/background.png").convert()
+screen.fill((173, 216, 230))
+
+fruit_list = load_fruit_images()
+curr_item = item_switcher(fruit_list)
+
+
 
 running = True
+item_y = 600
+
 while running:
+
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
+        elif e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            running = False
 
+    item_y = move_item(screen, curr_item, item_y)
 
-    screen.blit(bg, (0, 0))
     pygame.display.flip()
-    clock.tick(120)
+
+    clock.tick(60)
 
 
 pygame.quit()
---
+++ functions.py
@@ -0,0 +1,72 @@
+import pygame
+import random
+
+WINDOW_WIDTH = 650
+WINDOW_HEIGHT = 1000
+
+BG_COLOR = (173, 216, 230)
+
+ITEM_SPEED = 0.5
+
+
+
+
+
+def load_fruit_images() -> list[pygame.Surface]:
+    fruits = []
+
+    pear = pygame.image.load("Images/Items/pear.png").convert_alpha()
+    sca_pear = pygame.transform.scale(pear,
+                                    (pear.get_width() * 0.3,
+                                    pear.get_height() * 0.3))
+    fruits.append(sca_pear)
+
+    apple = pygame.image.load("Images/Items/apple.png").convert_alpha()
+    sca_apple = pygame.transform.scale(apple,
+                                       (apple.get_width() * 0.3,
+                                        apple.get_height() * 0.3))
+    fruits.append(sca_apple)
+
+    orange = pygame.image.load("Images/Items/orange.png").convert_alpha()
+    sca_orange = pygame.transform.scale(orange,
+                                       (orange.get_width() * 0.3,
+                                        orange.get_height() * 0.3))
+    fruits.append(sca_orange)
+
+    banana = pygame.image.load("Images/Items/banana.png").convert_alpha()
+    sca_banana = pygame.transform.scale(banana,
+                                       (banana.get_width() * 0.3,
+                                        banana.get_height() * 0.3))
+    fruits.append(sca_banana)
+
+    cherry = pygame.image.load("Images/Items/cherry.png").convert_alpha()
+    sca_cherry = pygame.transform.scale(cherry,
+                                       (cherry.get_width() * 0.3,
+                                        cherry.get_height() * 0.3))
+    fruits.append(sca_cherry)
+
+    bomb = pygame.image.load("Images/Items/bomb.png").convert_alpha()
+    sca_bomb = pygame.transform.scale(bomb,
+                                       (bomb.get_width() * 0.2,
+                                        bomb.get_height() * 0.2))
+    fruits.append(sca_bomb)
+    # add the other fruits...
+    
+    return fruits
+
+
+
+def move_item(screen: pygame.Surface, image: pygame.Surface, y: float) -> float:
+    y += ITEM_SPEED
+    screen.fill((BG_COLOR))
+    screen.blit(
+        image,
+        ((WINDOW_WIDTH / 2) - (image.get_width()/2), y)
+    )
+    return y
+
+
+
+def item_switcher(fruit_list: pygame.Surface) -> pygame.Surface:
+    return random.choice(fruit_list)
+    
