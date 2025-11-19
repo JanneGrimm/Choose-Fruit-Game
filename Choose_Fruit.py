0a6010dd13333121497b1871e33a551e77b998d4
Janne Aleksander Grimm <jannealeksandergrimm@mac.speedport.ip>
2025-11-20 00:41:42 +0100

    working on making fruit moving left via press of arrow key

--- Choose_Fruit.py
+++ Choose_Fruit.py
@@ -1,9 +1,9 @@
 import pygame
 import sys
 
-from functions import WINDOW_WIDTH, WINDOW_HEIGHT
-from functions import move_item, item_switcher, load_fruit_images
-
+from functions import WINDOW_WIDTH, WINDOW_HEIGHT, BG_SKY
+from functions import load_background, item_switcher, load_fruit_images
+from functions import move_item_horizontal, move_item_vertical
 
 
 
@@ -11,7 +11,10 @@ pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
-screen.fill((173, 216, 230))
+# Background
+screen.fill((BG_SKY))
+#bg_yard = load_background(screen)
+
 
 fruit_list = load_fruit_images()
 curr_item = item_switcher(fruit_list)
@@ -20,20 +23,36 @@ curr_item = item_switcher(fruit_list)
 
 running = True
 item_y = 600
+item_x = 600
 
 while running:
+    screen.fill((BG_SKY))
+    item_y = move_item_vertical(screen, curr_item, item_y)
+
+
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
-        elif e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
             running = False
+            
+        # i want to make the exact fruit variable to move left
+        # need to stop it going down
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_RIGHT:
+            item_x = move_item_horizontal(item_x, screen, curr_item)
 
-    item_y = move_item(screen, curr_item, item_y)
 
-    pygame.display.flip()
+
+
+    
+    #screen.blit(bg_yard, (0,500))
+
 
     clock.tick(60)
+    pygame.display.flip()
+
+    
 
 
 pygame.quit()
